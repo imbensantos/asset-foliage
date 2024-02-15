ee257bfde15de086fff91a5e6056188281be1d7f
Ben Santos <imbensantos@gmail.com>
2024-02-16 05:46:40 +0800

    Feat: Added Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import Icons from "./Icons";
+import NavItems from "./NavItems";
+
+type Props = {};
+
+function Navbar({}: Props) {
+  return (
+    <div className="sticky inset-x-0 top-0 z-50 h-16 bg-white">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* TODO: Mobile Nav */}
+
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.Logo className="h-10 w-10" />
+                </Link>
+              </div>
+
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
