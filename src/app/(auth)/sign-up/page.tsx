e9e8e7b89acc8900d063bc6c4674a23adc4785c5
Ben Santos <imbensantos@gmail.com>
2024-02-22 01:49:20 +0800

    Feat: Setup TRPC

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -15,6 +15,7 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from "@/lib/validators/account-credentials-validator";
+import { trpc } from "@/trpc/client";
 
 type Props = {};
 
@@ -26,6 +27,8 @@ function Page({}: Props) {
   } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
   });
+
+  const { data } = trpc.anyApiRoute.useQuery();
 
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
     // send data to the server
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { appRouter } from "./trpc";
+
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({
+  req, res
+})
 
 const start = async () => {
   const payload = await getPayloadClient({
@@ -15,15 +22,20 @@ const start = async () => {
     },
   });
 
+  app.use('/api/trpc', trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext
+  }))
+
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    // payload.logger.info("NextJS started");
+    payload.logger.info("NextJS started");
 
     app.listen(PORT, async () => {
-      // payload.logger.info(
-      //   `NextJS App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`,
-      // );
+      payload.logger.info(
+        `NextJS App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`,
+      );
     });
   });
 };
