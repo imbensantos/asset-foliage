e0d4270e8e8ceb69c32be8b518f035e9aaaa947f
Ben Santos <imbensantos@gmail.com>
2024-02-21 22:59:05 +0800

    Feat: Added signup page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Icons from "@/components/Icons";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+function Page({}: Props) {
+  return (
+    <>
+      <div className="container relative flex flex-col items-center justify-center pt-20 lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Icons.Logo className="h-20 w-20" />
+            <h1 className="text-2xl font-bold">Create an account</h1>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={() => {}}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    className={cn({
+                      "focus-visible:ring-red-500": true,
+                    })}
+                    placeholder="name@example.com"
+                  />
+                </div>
+
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    className={cn({
+                      "focus-visible:ring-red-500": true,
+                    })}
+                    placeholder="Password"
+                  />
+                </div>
+
+                <Button>Sign up</Button>
+                <p className="text-center text-sm font-normal">
+                  Already have an account?{" "}
+                  <Link
+                    className={buttonVariants({
+                      variant: "link",
+                      className: "gap-1.5 px-0 text-green-600",
+                    })}
+                    href="/sign-in"
+                  >
+                    Login
+                    <ArrowRight className="h-4 w-4" />
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Page;
