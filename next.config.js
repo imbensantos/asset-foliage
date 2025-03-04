ff65600b6245d4c116f3065b4916d9602fe2f2ee
Ben Santos <imbensantos@gmail.com>
2025-03-05 06:27:51 +0800

    Bug: Added remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,19 +2,31 @@
 module.exports = {
   images: {
     formats: ["image/avif", "image/webp"],
-    // remotePatterns: [
-    //   {
-    //     protocol: "http",
-    //     hostname: "localhost",
-    //     port: "3000",
-    //     pathname: "/storage/media/**",
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/storage/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assetfoliage.imbensantos.com",
+        pathname: "/storage/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assetfoliage.up.railway.app",
+        port: "8080",
+        pathname: "/storage/media/**",
+      }
+    ],
     domains: [
       "localhost",
       "imbensantos-assetfoliage.vercel.app",
       "assetfoliage.imbensantos.com",
       "asset-foliage.up.railway.app",
+      "assetfoliage.up.railway.app"
     ],
   },
 };
